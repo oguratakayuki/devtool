ecdc655a40741262e9dfb62a4d1195a826a19281
ogura takayuki <ogura@coobal.co.jp>
2016-09-01 11:24:25 +0900

    パフォーマンス比較スクリプト作成

--- web_analyze_tools/download.rb
+++ web_analyze_tools/download.rb
@@ -1,8 +1,13 @@
 require './tool'
-cmd = 'wget  --no-clobber --page-requisites --html-extension --convert-links \
---restrict-file-names=windows --domains \
-client3.coore-on-rails.co.jp.responsive-develop02.s-rep.net --no-parent \
-http://client3.coore-on-rails.co.jp.responsive-develop02.s-rep.net:8080'
+cmd = 'wget   \
+  -P results \
+  --page-requisites \
+  --html-extension \
+  --convert-links \
+  --restrict-file-names=windows \
+  --domains \
+  client3.coore-on-rails.co.jp.responsive-develop02.s-rep.net --no-parent \
+  http://client3.coore-on-rails.co.jp.responsive-develop02.s-rep.net:8080'
 
 # Length: 10468 (10K) [application/octet-stream]
 
@@ -11,23 +16,40 @@ o, e, s = Open3.capture3(cmd)
 
 es = e.split(/\n+/)
 sizes_groupby_mime = {}
+download_speeds = []
 ret = es.each do |line|
-  line.match(/^(Length.*)$/) do |matched|
-    if matched[1].match(/\s(\d*)\s/)
-      size_in_byte =  matched[1].match(/\s(\d*)\s/)[1].to_i
-      mime_type =  matched[1].match(/\s\[(.*)\]\z/)[1]
-      #puts mime_type
-      begin
-        sizes_groupby_mime[mime_type.to_sym] ||= 0
-      rescue => e
-        puts matched[1]
-        puts e.inspect
-        exit
+  if line.match(/^(Length.*)$/)
+    line.match(/^(Length.*)$/) do |matched|
+      if matched[1].match(/\s(\d*)\s/)
+        size_in_byte =  matched[1].match(/\s(\d*)\s/)[1].to_i
+        mime_type =  matched[1].match(/\s\[(.*)\]\z/)[1]
+        #puts mime_type
+        begin
+          sizes_groupby_mime[mime_type.to_sym] ||= 0
+        rescue => e
+          puts matched[1]
+          puts e.inspect
+          exit
+        end
+        sizes_groupby_mime[mime_type.to_sym] = sizes_groupby_mime[mime_type.to_sym] + size_in_byte
       end
-      sizes_groupby_mime[mime_type.to_sym] = sizes_groupby_mime[mime_type.to_sym] + size_in_byte
     end
-    #Length: 1126 (1.1K) [image/png]
-    #puts match
+  elsif line.match(/saved/)
+    line.match(/saved/) do |matched|
+      # 2016-09-01 10:32:27 (8.76 MB/s) - ‘results/example.com/assets/jquery-ui/ui-bg_highlight-soft_75.png’ saved [101/101]
+      # TODO (399 KB/s) をとってきて平均を出す
+      if temp.match(/\.[png|gif]/)
+        download_speeds[:images] << line
+      elsif temp.match(/\.html/)
+        download_speeds[:html] << line
+      elsif temp.match(/\.css/)
+        download_speeds[:css] << line
+      elsif temp.match(/\.js/)
+        download_speeds[:js] << line
+      else
+        download_speeds[:others] << line
+      end
+    end
   end
 end
 
@@ -41,5 +63,7 @@ end
 ret = e.match(/(FINISHED.*)/m)[1]
 puts ret
 
+
+puts download_speeds
 
 
