e49268379147fd493d43c5ea0efd5dc6950d2dc0
ogura takayuki <otn.ogura@gmail.com>
2017-07-13 14:29:31 +0900

    ファイル追加

---
+++ smarty_all_include_templates.rb
@@ -0,0 +1,53 @@
+
+require 'pathname'
+require "open3"
+
+
+def extract_include_paths(real_file_paths)
+  include_paths = []
+  real_file_paths.each do |real_file_path|
+    body = File.open(real_file_path, &:read)
+    body.each_line do |line|
+      if line.match(/include/) && matched = line.match(/"(.*\.html)"/)
+        include_paths << matched[1]
+      end
+    end
+  end
+  include_paths
+end
+
+
+
+def find_real_file_by_string(paths)
+  ret = []
+  #一番下のディレクトリ名を返す
+  #dir_name = Pathname.new(path).dirname.split.last.to_s
+  #file_name = Pathname.new(path).split.last.to_s
+  #find ./ -type d
+  paths.each do |path|
+    #o, e, s = Open3.capture3("find #{BASE} -type d >&2  | grep #{file_name}")
+    o, e, s = Open3.capture3("find #{BASE} -type f 2>/dev/null | grep #{path}")
+    files = o.split("\n")
+    files = files.delete_if{|file| !File.exists?(file) } 
+    ret = ret + files
+  end
+  ret
+end
+
+def main_loop(real_paths)
+  include_paths = extract_include_paths(real_paths)
+  extracted_realpaths = find_real_file_by_string(include_paths)
+  if extracted_realpaths.size > 0
+    @outputs << extracted_realpaths
+    main_loop extracted_realpaths
+  end
+end
+
+BASE = '/Users/oguratakayuki/work/zen/var/public_html/libs/templates/'
+@outputs = []
+root_file_path = ARGV[0]
+
+main_loop([root_file_path])
+
+temp = @outputs.uniq
+puts temp
